2bc7402ef9f2b27aafce921e7dd5dd9b9a0ab439
robsonbittencourt <robson.bittencourt@creditas.com>
2022-03-24 23:50:51 -0300

    Add log

--- src/main/java/com/bittencourt/docker/sample/ApiController.java
+++ src/main/java/com/bittencourt/docker/sample/ApiController.java
@@ -1,14 +1,21 @@
 package com.bittencourt.docker.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+    Logger logger = LoggerFactory.getLogger(RestController.class);
+
+
     @GetMapping(value = "/app-name")
     public String getAppName() {
         var appNameFromEnv = System.getenv("APP_NAME");
+        logger.info("Request received on " + appNameFromEnv + " app");
+
         return appNameFromEnv != null ? appNameFromEnv : "default-app-name";
     }
 
